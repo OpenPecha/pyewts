1b26758e6883b8121634002c6333d996fbb0339b
eroux <elie.roux@telecom-bretagne.eu>
2022-11-18 11:31:14 +0100

    add +s

--- pyewts/ACIP.py
+++ pyewts/ACIP.py
@@ -19,7 +19,8 @@ def ACIPtoEWTS(s):
     s = s.replace("#", "@##")
     s = s.replace("*", "@#")
     s = s.replace("\\", "?")
-    s = s.replace("^", "\\u0f38")
+    # the case will change
+    s = s.replace("^", "\\U0F38") # this is also sometimes encoded as 7
     s = s.replace(",", "/")
     s = s.replace("`", "!")
     s = s.replace("`", "!")
@@ -28,7 +29,7 @@ def ACIPtoEWTS(s):
     s = s.replace("TZ", "TS") 
     # - => .
     # GA-YAS is not the canonical form but is commonly found
-    s = re.sub(r"([BCDGHJKLMNPRSTVYZhdtn])A-", r"\1.", s)
+    s = re.sub(r"([BCDGHJKLMN'PRSTVYZhdtn])A-", r"\1.", s)
     s = s.replace("-", ".")
     #   - i => -I (we will reverse the case later)
     s = re.sub(r"A?i", "-I", s)
@@ -39,9 +40,9 @@ def ACIPtoEWTS(s):
     s = s.replace("o", "x")
     s = s.replace("%", "~x")
     #   - non-vowel + apostrophe + vowel (except i) => lower case vowel (idem)
-    s = re.sub(r"([BCDGHJKLMNPRSTVYZhdtn])'([AEOUI])", lambda m: m.group(1)+m.group(2).lower(), s)
+    s = re.sub(r"([BCDGHJKLMNPRSTVYZ'hdtn])'([AEOUI])", lambda m: m.group(1)+m.group(2).lower(), s)
     # this case is a bit complex, it's correcting the previous line for cases where A is the main letter
-    s = re.sub(r"(^|[^BCDGHJKLMNPRSTVYZhdtn])A'([AEOUI])", lambda m: m.group(1)+m.group(2).lower(), s)
+    s = re.sub(r"(^|[^BCDGHJKLMNPR'STVYZhdtn])A'([AEOUI])", lambda m: m.group(1)+m.group(2).lower(), s)
     #   - A + vowel => vowel
     s = re.sub(r"A([AEIOUaeiou])", r"\1", s)
     #   - sh => sH
@@ -54,8 +55,114 @@ def ACIPtoEWTS(s):
     s = s.replace("oo", "au")
     #   - : => H
     s = s.replace(":", "H")
-    #   - add pluss?
+    s = add_plus(s)
     return s
+
+# standard Tibetan roots (what's before the vowel), with an adjustment: all can take r, y or w or any combination at the end
+STD_TIB_PATTERN = re.compile(r"(bgl|dm|sm|sn|kl|dk|bk|bkl|rk|lk|sk|brk|bsk|kh|mkh|'kh|gl|dg|bg|mg|'g|rg|lg|sg|brg|bsg|ng|dng|mng|rng|lng|sng|brng|bsng|gc|bc|lc|ch|mch|'ch|mj|'j|rj|lj|brj|ny|gny|mny|rny|sny|brny|bsny|gt|bt|rt|lt|st|brt|blt|bst|th|mth|'th|gd|bd|md|'d|rd|ld|sd|brd|bld|bsd|gn|mn|rn|brn|bsn|dp|lp|sp|ph|'ph|bl|db|'b|rb|lb|sb|rm|ts|gts|bts|rts|sts|brts|bsts|tsh|mtsh|'tsh|dz|mdz|'dz|rdz|brdz|zh|gzh|bzh|zl|gz|bz|bzl|rl|brl|sh|gsh|bsh|sl|gs|bs|bsl|lh)[rwy]*")
+
+STD_TIB_STACKS_PREFIX = [
+    "bg",
+    "dm",
+    "dk",
+    "bk",
+    "brk",
+    "bsk",
+    "mkh",
+    "'kh",
+    "dg",
+    "bg",
+    "mg",
+    "'g",
+    "brg",
+    "bsg",
+    "dng",
+    "mng",
+    "brng",
+    "bsng",
+    "gc",
+    "bc",
+    "ch",
+    "mch",
+    "'ch",
+    "mj",
+    "'j",
+    "brj",
+    "gny",
+    "mny",
+    "brny",
+    "bsny",
+    "gt",
+    "bt",
+    "brt",
+    "blt",
+    "bst",
+    "mth",
+    "'th",
+    "gd",
+    "bd",
+    "md",
+    "'d",
+    "brd",
+    "bld",
+    "bsd",
+    "gn",
+    "mn",
+    "brn",
+    "bsn",
+    "dp",
+    "ph",
+    "'ph",
+    "bl",
+    "db",
+    "'b",
+    "gts",
+    "bts",
+    "brts",
+    "bsts",
+    "tsh",
+    "mtsh",
+    "'tsh",
+    "mdz",
+    "'dz",
+    "brdz",
+    "gzh",
+    "bzh",
+    "gz",
+    "bz",
+    "bzl",
+    "brl",
+    "gsh",
+    "bsh",
+    "gs",
+    "bs",
+    "bsl"
+]
+
+C_TOKEN_PATTERN = re.compile(r"zh|ny|dz|ts|tsh|ch|ph|th|sh|Sh|kh|ng|[bcdghjklmnprstwyz']")
+CONSONNANTS_PATTERN = re.compile(r"[bcdgjklm'nprstvyzhSDTN]+")
+
+STD_TIB_STACKS_PREFIX_TOKENS = []
+for s in STD_TIB_STACKS_PREFIX:
+    STD_TIB_STACKS_PREFIX_TOKENS.append(C_TOKEN_PATTERN.findall(s))
+
+def add_plus_to_consonnants(c):
+    if STD_TIB_PATTERN.fullmatch(c):
+        return c
+    # less common case, for Sanskrit, we have to add the +
+    c_tokens = C_TOKEN_PATTERN.findall(c)
+    # if we have a full match in STD_TIB_STACKS_PREFIX_TOKENS, we only add + after the first token
+    # because we consider the first letter is a prefix
+    if len(c_tokens) == 1:
+        return c
+    if c_tokens[:2] in STD_TIB_STACKS_PREFIX_TOKENS or c_tokens[:2] in STD_TIB_STACKS_PREFIX_TOKENS:
+        return c_tokens[0]+"+".join(c_tokens[1:])
+    return "+".join(c_tokens)
+
+def add_plus(src):
+    # for all matches of bcdgjklm'nprstvyzhSDTN
+    return CONSONNANTS_PATTERN.sub(lambda x: add_plus_to_consonnants(x.group(0)), src)
+
 
 def test_assert(orig, expected):
     res = ACIPtoEWTS(orig)
@@ -81,7 +188,8 @@ def testACIPtoEWTS():
     test_assert("L'i", "l-I")
     test_assert("AEE", "ai")
     test_assert("KEEm", "kaiM")
-    test_assert("KshA", "kSha")
+    test_assert("ARTHA", "ar+tha")
+    test_assert("DHA KshA", "d+ha k+Sha")
     test_assert("TSA TZA", "tsha tsa")
     test_assert("*, ,'PHAGS PA GSER 'OD DAM PA MDO SDE DBANG PO'I BSDUS PA BSHUGS SO, ,", "@#/ /'phags pa gser 'od dam pa mdo sde dbang po'i bsdus pa bshugs so/ /")
 
